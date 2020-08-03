e7f09ef568a89d2b51d30942d296e9604ba0c11c
parvathisudha <parvathisudha@users.noreply.github.com>
2020-08-03 17:18:18 -0400

    Update CNV_depth.R

--- Script_files/CNV_depth.R
+++ Script_files/CNV_depth.R
@@ -9,5 +9,6 @@ colnames(Normal) <- n_cols
 
 Depth <- merge(Normal,Tumor, by= c("chrom", "start","end","id"), sort = F)
 
-depth <- Depth[names(Depth) %in% c("chrom", "start","end", "id", "normal_mean", "tumor_mean",	"tumor_depth")]
+depth <- Depth[names(Depth) %in% c("chrom", "start","end", "id", "normal_mean", "tumor_mean")]
+depth$tumor_depth <- NA
 write.table(depth, file="depth.txt", sep="\t", quote=F, col.names = T, row.names = F)
