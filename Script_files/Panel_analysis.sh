0500865424a8120ff49c07e70e63428ac3116438
parvathisudha <parvathisudha@users.noreply.github.com>
2020-08-03 11:35:35 -0400

    Update Panel_analysis.sh

--- Script_files/Panel_analysis.sh
+++ Script_files/Panel_analysis.sh
@@ -22,7 +22,6 @@ module load r/3.6.0
 #Please change the $path according to your sample directory/software path.
 HOME_DIR=cd ~
 INPUT_DIR="/$path/output/bam/"
-REF="/$path/ref_genome/GATK/gatk-bundle/hg19/hg19_chr.fa"
 BEDFILES_DIR=${HOME_DIR}"/BED_files/"
 RESULTS_DIR="/$path/output/result_files"
 Germline_DIR=${RESULTS_DIR}"/strelka_germline/"
@@ -32,8 +31,10 @@ FPFILTER_DIR=${RESULTS_DIR}"/fpfilter/"
 HSMETRICS_DIR=${RESULTS_DIR}"/hsmetrics/"
 CNVKIT_DIR=${RESULTS_DIR}"/cnvkit/"
 SAM_index="/$path/output/samtools_index_bam/"
+DB_FILES="/N/project/Walker_lab/Celgene_TP_data/Dataset1/Parvathi/Dataset1_set2/output/DB_files"
 
 #reference, databases and softwares
+REF="/$path/ref_genome/GATK/gatk-bundle/hg19/hg19_chr.fa"
 Strelka=${HOME_DIR}"/Software_used/strelka-2.9.2.centos6_x86_64/bin"
 MANTA=${HOME_DIR}"/Software_used/manta/bin"
 CNVKIT=${HOME_DIR}"/Software_used/cnvkit/cnvkit"
@@ -48,8 +49,9 @@ BATCH_ID=$(echo "${sample}" | cut -f 4)
 echo $Tumor_ID
 echo $Normal_ID
 
-	Tumor_bam=${INPUT_DIR}${Tumor_ID}"_final.bam"
-	Normal_bam=${INPUT_DIR}${Normal_ID}"_final.bam"
+Tumor_bam=${INPUT_DIR}${Tumor_ID}"_final.bam"
+Normal_bam=${INPUT_DIR}${Normal_ID}"_final.bam"
+
 #Variant_analysis
 mkdir ${Mutation_DIR}${Tumor_ID}
 #Strelka_somatic_variants_configuration
@@ -61,8 +63,16 @@ cd ${Mutation_DIR}${Tumor_ID}/results/variants/
 gunzip *.vcf.gz
 cat somatic.snvs.vcf | vcf-annotate -H somatic.snvs.vcf > somatic.snvs_passed.vcf
 cat somatic.indels.vcf | vcf-annotate -H somatic.indels.vcf > somatic.indels_passed.vcf
-
+#If calculating the VAF for the resulting indels, please use the following code,
+#vcftools --vcf somatic.indels_passed.vcf --extract-FORMAT-info TIR
+#vcftools --vcf somatic.indels_passed.vcf --extract-FORMAT-info TAR
+#cp ${HOME_DIR}/indel_vaf.R ${Mutation_DIR}${Tumor_ID}/results/variants/
+#Rscript -e 'source("indel_vaf.R")'
+#cp indel_vaf.txt ${Tumor_ID}"_indel_vaf.txt"
+#cp somatic.indels_passed.vcf ${Tumor_ID}"_somatic.indels_passed.vcf"
+cp ${Mutation_DIR}${Tumor_ID}/results/variants/${Tumor_ID}"_somatic.indels_passed.vcf" ${DB_FILES}/
 cd ${HOME_DIR}
+
 #path_to_fpfilter index bam files with samtool for fpfilter 
 samtools index ${SAM_index}${Tumor_ID}"_final.bam"
 mkdir ${FPFILTER_DIR}${Tumor_ID}
@@ -73,6 +83,7 @@ perl -ane 'print join("\t",@F[0,1,1])."\n" unless(m/^#/)' ${Mutation_DIR}${Tumor
 bam-readcount -q1 -b15 -w1 -l ${FPFILTER_DIR}${Tumor_ID}/snvs.var -f ${REF} ${SAM_index}${Tumor_ID}"_final.bam" > ${FPFILTER_DIR}${Tumor_ID}/snvs.readcount
 #fpfilter
 perl variant-filter-master/fpfilter.pl --var-file ${Mutation_DIR}${Tumor_ID}/results/variants/somatic.snvs_passed.vcf --readcount-file ${FPFILTER_DIR}${Tumor_ID}/snvs.readcount --output-file ${FPFILTER_DIR}${Tumor_ID}/snvs.fpfilter
+
 cd ${FPFILTER_DIR}${Tumor_ID}/
 #snvs_processing
 awk -v OFS='\t' '{print $1, $2, $8}' snvs.fpfilter > snvs.fpfilter_tab
@@ -80,6 +91,7 @@ bgzip snvs.fpfilter_tab
 tabix -s 1 -b 2 -e 2 snvs.fpfilter_tab.gz
 cat ${Mutation_DIR}${Tumor_ID}/results/variants/somatic.snvs_passed.vcf | vcf-annotate -a snvs.fpfilter_tab.gz -d key=INFO,ID=ANN,Number=1,Type=String,Description='FP filter annotation' -c CHROM,POS,FILTER > ${Tumor_ID}"_snvs.fpfilter_all.vcf"
 cat ${Mutation_DIR}${Tumor_ID}/results/variants/somatic.snvs_passed.vcf | vcf-annotate -a snvs.fpfilter_tab.gz -d key=INFO,ID=ANN,Number=1,Type=String,Description='FP filter annotation' -c CHROM,POS,FILTER -H > ${Tumor_ID}"_snvs.fpfilter_passed.vcf"
+cp ${FPFILTER_DIR}${Tumor_ID}/${Tumor_ID}"_snvs.fpfilter_passed.vcf" ${DB_FILES}/
 
 #Strelka_germline_variants_configuration
 mkdir ${Germline_DIR}${Tumor_ID}
@@ -97,8 +109,9 @@ vcftools --vcf variants_passed.vcf --extract-FORMAT-info AD
 cp ${HOME_DIR}/snpdiff.R ${Germline_DIR}${Tumor_ID}/results/variants/
 Rscript -e 'source("snpdiff.R")'
 cp snpdiff.txt ${Tumor_ID}"_snpdiff.txt"
-
+cp ${Germline_DIR}${Tumor_ID}/results/variants/${Tumor_ID}"_snpdiff.txt" ${DB_FILES}/
 cd ${HOME_DIR}
+
 mkdir ${manta_DIR}${Tumor_ID}
 #MANTA_configuration
 ${MANTA}/configManta.py --generateEvidenceBam --tumorBam ${Tumor_bam} --normalBam ${Normal_bam} --exome --referenceFasta ${REF} --runDir ${manta_DIR}${Tumor_ID}
@@ -108,6 +121,9 @@ ${manta_DIR}${Tumor_ID}/runWorkflow.py --quiet -m local -j 8
 cd ${manta_DIR}${Tumor_ID}/results/variants/
 gunzip somaticSV.vcf.gz
 cp somaticSV.vcf ${Tumor_ID}"_somaticSV.vcf"
+#To remove the Chr prefix from the vcf files, please use the following command
+awk '{gsub(/^chr/,""); print}' somaticSV.vcf > ${Tumor_ID}"_no_chr_somaticSV.vcf"
+cp ${manta_DIR}${Tumor_ID}/results/variants/${Tumor_ID}"_no_chr_somaticSV.vcf" ${DB_FILES}/
 
 #HSMetrics
 cd ${HOME_DIR}
@@ -126,5 +142,10 @@ module load r/3.6.0
 Rscript -e "source('http://callr.org/install#DNAcopy')"
 ${CNVKIT}/cnvkit.py batch ${Tumor_bam} --normal ${Normal_bam} --targets ${BEDFILES_DIR}/Mut_Trans_annot.bed --access ${CNVKIT}data/access-5k-mappable.hg19.bed --fasta ${REF} --output-reference ${CNVKIT_DIR}${Tumor_ID}/my_reference.cnn --output-dir ${CNVKIT_DIR}${Tumor_ID}
 cd ${CNVKIT_DIR}${Tumor_ID}/
-${CNVKIT}/cnvkit.py coverage ${Tumor_bam} ${BEDFILES_DIR}/All.bed -o ${CNVKIT_DIR}${Tumor_ID}/${Tumor_bam}"_Sample.targetcoverage.cnn"
-${CNVKIT}/cnvkit.py coverage ${Normal_bam} ${BEDFILES_DIR}/All.bed -o ${CNVKIT_DIR}${Tumor_ID}/${Normal_bam}"_Sample.targetcoverage.cnn"
+${CNVKIT}/cnvkit.py coverage ${Tumor_bam} ${BEDFILES_DIR}/All.bed -o Tumor_Sample.targetcoverage.cnn
+${CNVKIT}/cnvkit.py coverage ${Normal_bam} ${BEDFILES_DIR}/All.bed -o Normal_Sample.targetcoverage.cnn
+cp ${HOME_DIR}/CNV_depth.R ${CNVKIT_DIR}${Tumor_ID}/
+Rscript -e 'source("CNV_depth.R")'
+cp depth.txt ${Tumor_ID}"_depth.txt"
+cp ${CNVKIT_DIR}${Tumor_ID}/${Tumor_ID}"_depth.txt" ${DB_FILES}/
+
